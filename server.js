47a4e096e3a5d061029de02b50bca8923ecba05f
kaushalchaudhary <33245797+kaushalchaudhary@users.noreply.github.com>
2018-03-02 21:34:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/;articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     // articleName=article-one
     // article[articleName]={}content object for article one
    var articleName = req.params.articleName;
