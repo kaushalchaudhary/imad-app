c523ddda9478e998a3a915f0c84680a4fab2b95f
kaushalchaudhary <33245797+kaushalchaudhary@users.noreply.github.com>
2018-03-02 19:09:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
    });
 app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
