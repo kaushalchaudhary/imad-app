ffe6a2933cba1392a5063df3c6607e3d05e52daf
kaushalchaudhary <33245797+kaushalchaudhary@users.noreply.github.com>
2018-03-03 17:02:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-
-
 app.use(morgan('combined'));
+
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
